08788c674186ad61d8cd261cfe0a45caf0dd5bae
Ahmed Eid <ahmedeid2504@gmail.com>
2024-03-07 00:47:50 +0200

    Adds hover effects on each card

--- components/Card.jsx
+++ components/Card.jsx
@@ -9,27 +9,30 @@ export default function Card(props) {
     }
     
     return (
-        <div className="card">
-            {
-                badgeText && 
-                <div className="card--badge">{badgeText}</div>
-            }
-            <img 
-                src={props.coverImg} 
-                alt="Cover image for card"
-                className="card--image" 
-            />
-            <div className="card--stats">
-                <img src="../images/star.png" className="card--star" />
-                <span>{props.stats.rating}</span>
-                <span className="gray">({props.stats.reviewCount}) • </span>
-                <span className="gray">{props.location}</span>
+        <a href="#" className='card-link'>
+            <div className="card">
+                {
+                    badgeText && 
+                    <div className="card--badge">{badgeText}</div>
+                }
+                <img 
+                    src={props.coverImg} 
+                    alt="Cover image for card"
+                    className="card--image" 
+                />
+                <div className="card--stats">
+                    <img src="../images/star.png" className="card--star" />
+                    <span>{props.stats.rating}</span>
+                    <span className="gray">({props.stats.reviewCount}) • </span>
+                    <span className="gray">{props.location}</span>
+                </div>
+                <p className="card--title">{props.title}</p>
+                <p className="card--price">
+                    <span className="bold">From ${props.price}</span> / person
+                </p>
             </div>
-            <p className="card--title">{props.title}</p>
-            <p className="card--price">
-                <span className="bold">From ${props.price}</span> / person
-            </p>
-        </div>
+        </a>
+        
     )
 }
 
