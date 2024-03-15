6fb0aa27ecbcb8871ead63f607290add294ba943
Ahmed Eid <ahmedeid2504@gmail.com>
2024-03-16 01:57:13 +0200

    changes styling of hero section

--- components/SectionHeader.jsx
+++ components/SectionHeader.jsx
@@ -3,9 +3,11 @@ import PropTypes from 'prop-types';
 const SectionHeader = (props) => {
     return (
         <div className='hero'>
+            <div className='hero-details'>
+                <h1 className="hero--header">{props.headerText}</h1>
+                <p className="hero--text">{props.paragraphText}</p>
+            </div>
             <img src={props.imageUrl} className="hero--photo" alt="hero-photo" />
-            <h1 className="hero--header">{props.headerText}</h1>
-            <p className="hero--text">{props.paragraphText}</p>
         </div>
     );
 }
