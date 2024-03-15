fcb1f25ddd03a0b0771fc0e9cd105f9f3d7c9846
Ahmed Eid <ahmedeid2504@gmail.com>
2024-03-15 02:16:20 +0200

    adds styling to the modal and new button for booking an event

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -1,25 +1,41 @@
 import PropTypes from 'prop-types';
-
+import { useEffect, useRef } from 'react';
 const Modal = ({ handleClose, title, coverImg, stats, location, description, price }) => {
+    const modalRef = useRef(null);
+
+    // Close modal when user clicks outside of it
+    const handleClickOutside = (event) => {
+        if (modalRef.current && !modalRef.current.contains(event.target)) {
+            handleClose();
+        }
+    };
+    useEffect(() => {
+        // Add event listener when component mounts
+        document.addEventListener('mousedown', handleClickOutside);
+
+        return () => {
+            // Remove event listener when component unmounts
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+    
     return (
         <div className="modal">
-            <div className="modal-content">
-                <h2>{title}</h2>
-                <img src={coverImg} alt="Cover image for card" className="card--image" />
-                <div className="card--stats">
-                    <img src="../images/star.png" className="card--star" alt="Star" />
-                    <span>{stats.rating}</span>
-                    <span className="gray">({stats.reviewCount}) • </span>
-                    <span className="gray">{location}</span>
+            <div className="modal-content" ref={modalRef}>
+                <div>
+                    <button className="close-btn" onClick={handleClose}>
+                        Close
+                    </button>
+                    <img src={coverImg} alt="Cover image for card" className="modal-image" />
                 </div>
-                <p className="card--title">{title}</p>
-                <p className="card--price">
-                    <span className="bold">From ${price}</span> / person
-                </p>
-                <p>{description}</p>
-                <button className="close-btn" onClick={handleClose}>
-                    Close
-                </button>
+                <div className='modal-details'>
+                    <h2>{title}</h2>
+                    <p>
+                        <span className="bold">From ${price}</span> / person
+                    </p>
+                    <p>{description}</p>
+                    <button>Book Now</button>
+                </div>
             </div>
         </div>
     );
